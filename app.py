6248a0c9d26509d57f89d0f9faae920305c8c6f1
yujitakai <yuji21eagles0912@yahoo.co.jp>
2019-06-05 23:09:09 +0900

    Second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return render_template("hello.html", message="天才")
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == "POST":
+        comment = request.form['comment']
+        comment2 = request.form['comment2']
+        data = comment + comment2
+
+
+
+    return render_template("result.html", prediction = data)
+
 
 
 if __name__ == '__main__':
