6538dc913a01091d625da442e500e55984d29e06
yoshikiw <yoshiki.w7@gmail.com>
2019-06-06 23:51:42 +0900

    ml part working :)

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, ENGLISH_STOP_WORDS
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+import pickle
 
 app = Flask(__name__)
 
@@ -10,16 +18,38 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    df = pd.read_csv("text.csv")
+    target = df["Category"]
+    text = df["Message"]
+    vect = CountVectorizer(token_pattern=r"\b\w[\w']+\b",stop_words="english")
+    cv_text = vect.fit_transform(text)
+    model = RandomForestClassifier()
+    model.fit(cv_text, target)
+    # pickle.dump(model, open("model.sav", 'wb'))
+    # model = pickle.load(open("model.sav",'rb'))
+
+
     if request.method == "POST":
         comment = request.form['comment']
-        comment2 = request.form['comment2']
-        data = comment + comment2
+        data = [comment]
+        vec = vect.transform(data).toarray()
+        #print(vec)
+        pred = model.predict(vec)
 
 
 
-    return render_template("result.html", prediction = data)
+
+    #     comment2 = request.form['comment2']
+    #     data = comment + comment2
+
+
+
+
+    return render_template("result.html", prediction = pred)
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
